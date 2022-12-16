7cadadf4de64a9ce2222bd10db851a7f9a2c448f
Demis Lavrentidis <demis.lavrentidis@openbet.com>
2022-12-16 16:24:31 +0000

    -added custom backNavigation -disabled swipe gesture sliding by Lock mode

--- app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.android.sports.databinding.FragmentSportsListBinding
 
 /**
@@ -30,6 +32,32 @@ import com.example.android.sports.databinding.FragmentSportsListBinding
  */
 
 class SportsListFragment : Fragment() {
+    class SportsListOnBackPressedCallback (
+        private val slidingPaneLayout: SlidingPaneLayout
+        ): OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+        SlidingPaneLayout.PanelSlideListener {
+
+        init {
+            slidingPaneLayout.addPanelSlideListener(this)
+        }
+
+        override fun handleOnBackPressed() {
+            slidingPaneLayout.closePane()
+        }
+
+        override fun onPanelSlide(panel: View, slideOffset: Float) {
+
+        }
+
+        override fun onPanelOpened(panel: View) {
+            isEnabled = true
+
+        }
+
+        override fun onPanelClosed(panel: View) {
+            isEnabled = false
+        }
+    }
 
     private val sportsViewModel: SportsViewModel by activityViewModels()
 
@@ -52,6 +80,15 @@ class SportsListFragment : Fragment() {
             // Navigate to the details screen
            binding.slidingPaneLayout.openPane()
         }
+        val slidingPaneLayout = binding.slidingPaneLayout
+        slidingPaneLayout.lockMode = SlidingPaneLayout.LOCK_MODE_LOCKED
+        // Connect the SlidingPaneLayout to the system back button.
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            SportsListOnBackPressedCallback(slidingPaneLayout)
+        )
+
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
     }
